4d996da1a86bb71f70ac1c8b5dfc071ad398551a
rigwild <me@rigwild.dev>
2021-11-02 23:21:17 +0100

    Delete msg from db in place, add msg count

--- README.md
+++ README.md
@@ -18,17 +18,21 @@ pnpm build
 
 You can pass the time as a [ms](https://github.com/vercel/ms) string.
 
+**Note:** A bot can't delete a message that is older than 48 hours.
+
 ```sh
-TG_TOKEN=<telegram_token> DELETE_AFTER=2d node dist/index.js
+TG_TOKEN=<telegram_token> DELETE_AFTER_TIME=30h DELETE_AFTER_MESSAGES_COUNT=3500 node dist/index.js
 ```
 
 Using [PM2](https://pm2.keymetrics.io/).
 
 ```sh
 TG_TOKEN=<telegram_token> \
-DELETE_AFTER=2d \
+DELETE_AFTER_TIME=30h \
+DELETE_AFTER_MESSAGES_COUNT=3500 \
+DELETE_AFTER=30h \
 NODE_ENV=production \
-pm2 start --max-memory-restart 150M --name "telegram-history-clean-bot" dist/index.js
+pm2 start --max-memory-restart 150M --time --name "telegram-history-clean-bot" dist/index.js
 ```
 
 ## License
--- index.ts
+++ index.ts
@@ -5,9 +5,11 @@ import { fileURLToPath } from 'url'
 import TelegramBot from 'node-telegram-bot-api'
 
 if (!process.env.TG_TOKEN) throw new Error('TG_TOKEN is missing')
-if (!process.env.DELETE_AFTER) throw new Error('DELETE_AFTER is missing')
+if (!process.env.DELETE_AFTER_TIME) throw new Error('DELETE_AFTER_TIME is missing')
+if (!process.env.DELETE_AFTER_MESSAGES_COUNT) throw new Error('DELETE_AFTER_MESSAGES_COUNT is missing')
 
-const DELETE_AFTER_MS = (await import('ms')).default(process.env.DELETE_AFTER)
+const DELETE_AFTER_TIME_MS = (await import('ms')).default(process.env.DELETE_AFTER_TIME)
+const DELETE_AFTER_MESSAGES_COUNT = +process.env.DELETE_AFTER_TIME
 
 type Data = { messages: Array<{ chatId: string; messageId: string; timestamp: number }> }
 const __dirname = dirname(fileURLToPath(import.meta.url))
@@ -24,26 +26,32 @@ setInterval(async () => {
   if (isDeletingMessagesLock || !db.data) return
 
   isDeletingMessagesLock = true
-  const deletedMessagesIds: string[] = []
   try {
     const messages = db.data.messages!
-    for (let i = 0; i < messages.length && messages[i].timestamp < Date.now() - DELETE_AFTER_MS; i++) {
-      await bot.deleteMessage(messages[i].chatId, messages[i].messageId)
-      console.log(`Deleted message id=${messages[i].messageId}`)
-      deletedMessagesIds.push(messages[i].messageId)
+    for (
+      let i = 0;
+      i < messages.length &&
+      (messages.length > DELETE_AFTER_MESSAGES_COUNT || messages[i].timestamp < Date.now() - DELETE_AFTER_TIME_MS);
+      i++
+    ) {
+      try {
+        await bot.deleteMessage(messages[i].chatId, messages[i].messageId)
+        console.log(`Deleted message id=${messages[i].messageId}`)
+      } catch (err: any) {
+        console.log(`Failed to delete message id=${messages[i].messageId}`)
+        console.error(err.message)
+      }
+      db.data.messages.splice(i, 1)
+      await db.write()
+      i--
       await new Promise(res => setTimeout(res, 100))
     }
   } catch (err: any) {
     console.error(err.message)
   } finally {
-    if (deletedMessagesIds.length > 0) {
-      console.log(`Finished deleting ${deletedMessagesIds.length} messages`)
-      db.data.messages = db.data.messages.filter(x => !deletedMessagesIds.includes(x.messageId))
-      await db.write()
-    }
     isDeletingMessagesLock = false
   }
-}, 30_000)
+}, 5_000)
 
 bot.on('message', async msg => {
   db.data?.messages.push({ chatId: msg.chat.id + '', messageId: msg.message_id + '', timestamp: Date.now() })
